5fcc4b88393a453fdb3e41fd61adcdc11591396d
dferreto <diferretomo@est.utn.ac.cr>
2023-07-15 08:44:33 -0600

    Actualización de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'mantenimiento', loadChildren: () => import('./pages/mantenimientos/mantenimientos.module').then(m => m.MantenimientosModule) }, { path: 'mantenimiento', loadChildren: () => import('./pages/mantenimientos/clientes/clientes.module').then(m => m.ClientesModule) }];
+const routes: Routes = [{ path: 'mantenimiento', loadChildren: () => import('./pages/mantenimientos/mantenimientos.module').then(m => m.MantenimientosModule) }, { path: 'mantenimiento', loadChildren: () => import('./pages/mantenimientos/clientes/clientes.module').then(m => m.ClientesModule) }, { path: 'login', loadChildren: () => import('./pages/seguridad/seguridad.module').then(m => m.SeguridadModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
